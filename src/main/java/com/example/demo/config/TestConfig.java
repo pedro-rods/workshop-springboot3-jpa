fbc098bb6f3c9f9d3ad9200e37dda20a0d661481
Pedro Rodrigues <pedroar2003@gmail.com>
2023-12-10 13:35:38 -0300

    Payment, one-to-one association

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.demo.entities.Category;
 import com.example.demo.entities.Order;
 import com.example.demo.entities.OrderItem;
+import com.example.demo.entities.Payment;
 import com.example.demo.entities.Product;
 import com.example.demo.entities.User;
 import com.example.demo.entities.enums.OrderStatus;
@@ -37,7 +38,7 @@ public class TestConfig implements CommandLineRunner {
 	private ProductRepository productRepository;
 	
 	@Autowired
-	private OrderItemRepository orderItemRespository; 
+	private OrderItemRepository orderItemRepository; 
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -79,9 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
-		orderItemRespository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment (null, Instant.parse("2019-06-20T21:53:07Z"), o1 ); 
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1); 
 		
 	
 	}
